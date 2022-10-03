4e5690a71e48fddae105b56695b409a76aac7901
Erick Huamani <dev.erick95@gmail.com>
2022-10-03 18:48:16 -0500

    Cambio de url de api

--- config/app-config.js
+++ config/app-config.js
@@ -1,7 +1,7 @@
 const config = {
     api: {
         // url: 'http://localhost:3000',
-        url: 'http://167.99.155.76:3000',
+        url: 'http://67.207.84.147:3000',
         urlDev: 'http://localhost:3000/'
     }
 }
