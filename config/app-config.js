788c81310d81dde97792a1b8ee199d1f2595bbde
Erick Huamani <dev.erick95@gmail.com>
2022-10-04 01:19:12 -0500

    Se cambió host de servidor

--- config/app-config.js
+++ config/app-config.js
@@ -1,7 +1,7 @@
 const config = {
     api: {
         // url: 'http://localhost:3000',
-        url: 'http://67.207.84.147:3000',
+        url: 'http://192.241.129.91:3000',
         urlDev: 'http://localhost:3000/'
     }
 }
