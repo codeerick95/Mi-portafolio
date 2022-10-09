25af4c0f49f28c74e4cc78d01b8e0a95db1dad18
Erick Huamani <dev.erick95@gmail.com>
2022-10-08 21:29:10 -0500

    Actualización de host de producción

--- config/app-config.js
+++ config/app-config.js
@@ -1,7 +1,7 @@
 const config = {
     api: {
         // url: 'http://localhost:3000',
-        url: 'http://192.241.129.91:3000',
+        url: 'http://157.230.209.105:3000',
         urlDev: 'http://localhost:3000/'
     }
 }
