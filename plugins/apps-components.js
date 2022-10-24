19a6387c387747d57a18dfeee762e33685d790a2
Erick Huamani <dev.erick95@gmail.com>
2022-10-24 02:40:12 -0500

    Eliminación de NuxtUploadImages

--- plugins/apps-components.js
+++ plugins/apps-components.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
-import { AdminLayout, NuxtUploadImages, AdminSearch, FormError } from 'apps-components'
+import { AdminLayout, AdminSearch, FormError } from 'apps-components'
 
 Vue.component('AdminLayout', AdminLayout)
-Vue.component('NuxtUploadImages', NuxtUploadImages)
+// Vue.component('NuxtUploadImages', NuxtUploadImages)
 Vue.component('AdminSearch', AdminSearch)
 Vue.component('FormError', FormError)
